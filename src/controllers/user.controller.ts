679a01c3aacc1146e43c8c5b8a6bc2f5cc62975f
ayush-0012 <ayushbisht0012@gmail.com>
2025-05-27 00:11:08 +0530

    created a controller to fetch all the files

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -31,3 +31,36 @@ export async function SignIn(req: Request, res: Response): Promise<any> {
       .json({ message: "unable to create user", error: error.message });
   }
 }
+
+export async function fetchUserFiles(
+  req: Request,
+  res: Response
+): Promise<any> {
+  const { userId } = req.query;
+
+  console.log("req.query", req.query);
+
+  console.log("userId from query params", userId);
+
+  if (!userId || Array.isArray(userId)) {
+    return res.status(400).json({ message: "Valid userId is required" });
+  }
+
+  try {
+    const files = await prisma.file.findMany({
+      where: {
+        userId: userId.toString(),
+      },
+    });
+
+    return res.status(200).json({
+      message: "Files fetched successfully",
+      files,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: "Error occurred while fetching files",
+      error,
+    });
+  }
+}
